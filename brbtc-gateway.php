70cbfdac03ff1faaffb66b9e2e5f76babcddb799
Erick Crus <erickcrus@gmail.com>
2023-08-09 21:50:33 -0300

    initial commit

---
+++ brbtc-gateway.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: BRBTC Gateway
+ * Plugin URI: https://github.com/brbtcoficial/brbtc-gateway
+ * Description: Official BRBTC Gateway for WooCommerce
+ * Author: Brasil Bitcoin
+ * Author URI: https://brasilbitcoin.com.br
+ * Version: 0.0.1
+ */
